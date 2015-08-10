5defe3e188ea3882bceeaab6cbfdaed40fe3bdb1
Jim Veneskey <github@strifecrafter.com>
2015-08-10 06:48:14 -0400

    Experimenting with asynchronous requests

---
+++ 01-03/ajax/script.js
@@ -0,0 +1,12 @@
+// new style - using asynch requests
+var request = new XMLHttpRequest();
+request.open('GET', 'data.txt');
+// check for the onready state change
+request.onreadystatechange = function () {
+    if ( (request.readyState === 4) && (request.status === 200) ) {
+        console.log(request);
+        document.writeln(request.responseText);
+    }	
+}
+
+request.send();
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # LyndaJavascriptAjax
+
+Start here the next time:
+http://www.lynda.com/Ajax-tutorials/Scripting-backwards-compatibility/114900/120866-4.html
