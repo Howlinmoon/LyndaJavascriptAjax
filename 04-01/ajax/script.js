00128797b0bcc2cc7362dfc21442a90db4311b5a
Jim Veneskey <github@strifecrafter.com>
2015-08-12 12:58:10 -0400

    Started part 3 - almost finished

--- 04-01/ajax/script.js
+++ 04-01/ajax/script.js
@@ -1,15 +1,18 @@
-$.getJSON('data.json', function(data) {
-    console.log(data);
-    var output = "<ul class='searchresults'>";
-    $.each(data, function(key, val) {
-        output += '<li>';
-        output += '<h2>' + val.name + '</h2>';
-        output += '<img src="images/' + val.shortname + '_tn.jpg" alt="' + val.name + '" />';
-        output += '<p>' + val.bio + '</p>';
-        
-        
-        output += '</li>';
+// Placing the entire retrieval and display inside of a keyup event handler
+$('#search').keyup(function() {
+    $.getJSON('data.json', function(data) {
+        console.log(data);
+        var output = "<ul class='searchresults'>";
+        $.each(data, function(key, val) {
+            output += '<li>';
+            output += '<h2>' + val.name + '</h2>';
+            output += '<img src="images/' + val.shortname + '_tn.jpg" alt="' + val.name + '" />';
+            output += '<p>' + val.bio + '</p>';
+
+
+            output += '</li>';
+        });
+        output += '</ul>';
+        $('#update').html(output);
     });
-    output += '</ul>';
-    $('#update').html(output);
 });
