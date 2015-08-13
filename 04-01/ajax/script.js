dda4a4f39861b312b8982faac42a24ff9034ae34
Jim Veneskey <github@strifecrafter.com>
2015-08-13 06:23:16 -0400

    Implementing a basic search using regular expressions

--- 04-01/ajax/script.js
+++ 04-01/ajax/script.js
@@ -1,16 +1,23 @@
 // Placing the entire retrieval and display inside of a keyup event handler
 $('#search').keyup(function() {
+    var searchField = $('#search').val();
+    console.log(searchField);
     $.getJSON('data.json', function(data) {
         console.log(data);
+        // A very simplistic regexp
+        var myExp = new RegExp(searchField, "i");
         var output = "<ul class='searchresults'>";
         $.each(data, function(key, val) {
-            output += '<li>';
-            output += '<h2>' + val.name + '</h2>';
-            output += '<img src="images/' + val.shortname + '_tn.jpg" alt="' + val.name + '" />';
-            output += '<p>' + val.bio + '</p>';
-
-
-            output += '</li>';
+            
+            if ((val.name.search(myExp) != -1) ||
+                (val.bio.search(myExp) != -1)) {
+            
+                output += '<li>';
+                output += '<h2>' + val.name + '</h2>';
+                output += '<img src="images/' + val.shortname + '_tn.jpg" alt="' + val.name + '" />';
+                output += '<p>' + val.bio + '</p>';
+                output += '</li>';
+            }
         });
         output += '</ul>';
         $('#update').html(output);
